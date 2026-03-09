15a313cd8e092ee3ff1e5a3c2c0fec9aa07255ad
GAURANG SHARMA <gaurangg477@gmail.com>
2026-03-09 23:20:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Read CSV file
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+
+# Create dictionary from CSV
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+while True:
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(output_list)
+        break
